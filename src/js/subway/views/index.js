50ba687c919e46d2d82d1fcdecbc464ec56daefa
zereight <fullstackmachinelearning@gmail.com>
2021-03-19 22:46:46 +0900

    기능 ui 출력

--- src/js/subway/views/index.js
+++ src/js/subway/views/index.js
@@ -1,11 +1,10 @@
-import { stateManager } from '../../@shared/models/StateManager.js';
 import { root } from './templates/root.js';
 import { signIn } from './templates/signIn.js';
 import { signUp } from './templates/signUp.js';
 import { stationManage } from './templates/stationManage.js';
 import { lineManage } from './templates/lineManage.js';
 import { sectionManage } from './templates/sectionManage.js';
-import { STATE_KEY, ROUTE } from '../constants/constants.js';
+import { ROUTE } from '../constants/constants.js';
 import { parseToElements } from '../../@shared/utils/dom.js';
 import { $ } from '../../@shared/utils';
 
@@ -19,9 +18,9 @@ export const contentElements = {
   [ROUTE.ROOT]: $('#content', parseToElements(root)),
   [ROUTE.SIGNIN]: $('#content', parseToElements(signIn)),
   [ROUTE.SIGNUP]: $('#content', parseToElements(signUp)),
-  // [ROUTE.STATIONS]: parseToElements(stationManage),
-  // [ROUTE.LINES]: parseToElements(lineManage),
-  // [ROUTE.SECTIONS]: parseToElements(sectionManage),
+  [ROUTE.STATIONS]: $('#content', parseToElements(stationManage)),
+  [ROUTE.LINES]: $('#content', parseToElements(lineManage)),
+  [ROUTE.SECTIONS]: $('#content', parseToElements(sectionManage)),
   //   map: mapDisplay
   //   search:pathFind
 };
